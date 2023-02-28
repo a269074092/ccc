a413795cc82616b7e021e45fb236d13a15800dfb
Your Name <you@example.com>
2023-03-01 00:43:42 +0800

    23

--- app/Http/Controllers/CounterController.php
+++ app/Http/Controllers/CounterController.php
@@ -19,59 +19,9 @@ use Illuminate\Support\Facades\Log;
 
 class CounterController extends Controller
 {
-    function geturl($url){
-        $headerArray =array("Content-type:application/json;","Accept:application/json");
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch,CURLOPT_HTTPHEADER,$headerArray);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        $output = json_decode($output,true);
-        return $output;
+    public function wxtest(){
+        echo 21;
     }
-    public function getwx(){
-
-
-        $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx35427d448aa7791d&secret=e8720b20a5ff6cf11cab571a0f3fabeb' ;
-        $res1 = $this->geturl($url) ;
-
-        $access_token = isset($res1['access_token'])?$res1['access_token']:'' ;
-        //echo 'access_token'.$access_token ;echo "<br>" ;echo '<br>' ;
-        $url2 = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token='.$access_token.'&type=jsapi';
-
-        $res2 = $this->geturl($url2) ;
-        //var_dump($res2);echo '<br>' ;echo '<br>' ;
-        $ticket = isset($res2['ticket'])?$res2['ticket']:'' ;
-
-        $stringarr = array(
-            'jsapi_ticket'  => $ticket,
-            'noncestr'      => 'Wm3WZYTPz0wzccnW' ,
-            'timestamp'     => time() ,
-            'url'           => 'http://mp.weixin.qq.com?params=value'
-        );
-        //var_dump($stringarr);
-        //echo "<br>" ;echo "<br>" ;
-        $string1 = '' ;
-        foreach ($stringarr as $key => $value) {
-            if ($key == 'jsapi_ticket') {
-                $string1 = $string1.$key.'='.$value ;
-            }else {
-                $string1 = $string1."&".$key.'='.$value ;
-            }
-        }
-
-       // var_dump('$string1: <br>'.$string1);echo '<br>' ;echo '<br>' ;
-//$string1='jsapi_ticket=O3SMpm8bG7kJnF36aXbe84xSqh4Bqpw-3Tz_LQQXura4wIF878meBbEgNap7iSoFW0TP4Qt08dTvWJPgBobPFA&noncestr=Wm3WZYTPz0wzccnW&timestamp=1677209742&url=http://mp.weixin.qq.com?params=value' ;
-        $signature = sha1($string1) ;
-        $stringarr['signature']=$signature ;
-        //echo '$signature:<br>'.$signature ; echo '<br>' ; echo '<br>' ;
-
-        return view('counter1',$stringarr);
-    }
-
     /**
      * 获取todo list
      * @return Json
@@ -119,14 +69,14 @@ class CounterController extends Controller
                 }else {
                     $count = $data["count"] + 1;
                 }
-    
+
                 $counters = new Counters;
                 $counters->updateOrCreate(['id' => 1], ["count" => $count]);
             }else if ($action == "clear") {
                 Counters::destroy(1);
                 $count = 0;
             }else {
-                //throw '参数action错误';
+                throw '参数action错误';
             }
 
             $res = [
